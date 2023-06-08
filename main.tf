8de2b18c9fb2e2bf9a895650a6a1089d83e3e185
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-06-08 09:46:18 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.59.0"
+    }
+  }
+}
+
+provider "azurerm" {
+   subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+   client_id = "1244ff47-5233-442c-b8df-b761e220bc23"
+   client_secret = "YtY8Q~Q6kLoKDGGGEn3lGlILr--HZ5EsbJwOGba-"
+   tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+   skip_provider_registration = true
+   features {}
+}
+
+resource "azurerm_resource_group" "jktfrg" {
+  name     = "example-resources"
+  location = "francecentral"
+}
+
+resource "azurerm_virtual_network" "jktfvm" {
+  name                = "jknetwork"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.jktfrg.location
+  resource_group_name = azurerm_resource_group.jktfrg.name
+}
+
+resource "azurerm_subnet" "jktfsub" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.jktfrg.name
+  virtual_network_name = azurerm_virtual_network.jktfvm.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "jktfnic" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.jktfrg.location
+  resource_group_name = azurerm_resource_group.jktfrg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.jktfsub.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "jktfvm" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.jktfrg.name
+  location            = azurerm_resource_group.jktfrg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.jktfnic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
+  }
+}
