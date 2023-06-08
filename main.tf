9f88c8b0ca762f6206b3945e03045bbc6f7ed8dc
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-06-08 15:34:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "azurerm" {
    subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
-   #client_id = "1244ff47-5233-442c-b8df-b761e220bc23"
-   #client_secret = "YtY8Q~Q6kLoKDGGGEn3lGlILr--HZ5EsbJwOGba-"
+   client_id = "1244ff47-5233-442c-b8df-b761e220bc23"
+   client_secret = "YtY8Q~Q6kLoKDGGGEn3lGlILr--HZ5EsbJwOGba-"
    tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
    skip_provider_registration = true
    features {}
