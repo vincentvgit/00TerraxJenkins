b16dd0c386f34adfb11c60c13af81eebf1c11e79
vincentvgit <127845446+vincentvgit@users.noreply.github.com>
2023-06-09 09:41:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "azurerm" {
    subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
-   client_id = "384ba4e4-fc1a-47f4-bf42-acec6fad1e82"
-   client_secret = "lxk8Q~LfBuObGY4frVfc6B_e4Nqf6UMahS-BdaD-"
+   client_id = "f5b9dcdb-f79a-439b-a393-e1bb9a6a6de9"
+   client_secret = "Px88Q~z~MH3PFAu23fI5uhvvwpFccL2Jt0Zr7c_7"
    tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
    skip_provider_registration = true
    features {}
